0a1cf12da19b26514ee428f9800e9ae5d01a2cd2
shinigami <krzys.stasiowski@gmail.com>
2017-11-11 19:32:53 +0100

    komentarze

--- lab5/zad5-d-d.py
+++ lab5/zad5-d-d.py
@@ -3,45 +3,41 @@
 """
 Created on Mon Nov  6 11:49:26 2017
 
-@author: shinigami
+@author: Krzysztof Stasiowski
+WEAIiIB - Informatyka
+WDI
+liczenie liczb pierwszych metodą sita Eratostenesa
 """
 
 import time
 import math
+import sys
 
 def pierwsze(n):
-   
+    #deklaracja tablicy, pokazującej czy dana liczba jest pierwsza
     pierwsze = [False]*2 + [True]*(n-2)
+    #Deklaracja tablicy do przechowywania końcowych liczb pierwszych 
     ppierwsze =[0]*n
-    ppi=0;
-    d=math.sqrt(n)
+    ppi=0;# licznik liczb pierwszych
+    d=math.sqrt(n) #obliczenie pierwiastka z n, musimy sprawdzić jedynie liczbymniejsze od tego pierwiastka
 
-    for (i,p) in enumerate(pierwsze):
+    for (i,p) in enumerate(pierwsze):#przechodzimy przez listę liczbpierwszych (i - sprawdzana liczba, p - czy jest pierwsza)
         if(i>d):
-            break
+            break #zakończenie sprawdzania jeżeli i > pierwiastka z n
         if(not p):
-            continue
-        ppierwsze[ppi]=i
-        ppi+=1;
+            continue # pominięcie jeśeli i nie jest liczbą pierwszą
+        ppierwsze[ppi]=i #dodanie liczby pierwszej do listy liczb pierwszych
+        ppi+=1;#zwiększenie licznika liczb pierwszych
         for delete in range(i*i,n,i):
-            pierwsze[delete]=False
-    
-    
-    
-    return ppierwsze[0:ppi]
-    
-    #zwracamy wszyskie liczby pierwsze
+            pierwsze[delete]=False#usunięcie wszyskich wielokrotności liczby pierwszej
+        
+    return ppierwsze[0:ppi] #zwracamy wszyskie liczby pierwsze
 
 
-n = (int)(input())
+n = (int)(sys.argv[1]) #pobranie liczby sprawdzanych liczb jako argument programu
 
-#pobieramy czas praed wykonaniem funkcji
-times = []
-for i in range(2):
-    start_time = time.time()
-    p=pierwsze(n)
-    times.append((time.time() - start_time))
-    print(i)
-    print(p)
-print("--- {} sseconds ---".format(sum(times)/len(times)))
+#pobieramy czas przed wykonaniem funkcji
+start_time = time.time()
+p=pierwsze(n)
+print("{}   {}".format(n,((time.time() - start_time))))
 #wyświetlamy różnicę czasu
